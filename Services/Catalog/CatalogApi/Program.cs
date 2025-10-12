ad92177934945255f97df489afb06a86ef3bcdba
dell <dell@DESKTOP-J4RA8G6>
2025-10-12 16:38:35 +0200

    Add project files.

---
+++ Services/Catalog/CatalogApi/Program.cs
@@ -0,0 +1,14 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+app.MapCarter();
+app.Run();
